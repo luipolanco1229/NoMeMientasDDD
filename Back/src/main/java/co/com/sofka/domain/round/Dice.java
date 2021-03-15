be0d2bba4516bdf2f97490deea6fb8aeb27a4710
luipolanco1229 <Luipolanco1229>
2021-03-14 22:57:37 -0500

    Se empezaron a hacer las bases del cuarto caso de uso de lanzar dados

--- Back/src/main/java/co/com/sofka/domain/round/Dice.java
+++ Back/src/main/java/co/com/sofka/domain/round/Dice.java
@@ -4,6 +4,7 @@ import co.com.sofka.domain.generic.Entity;
 import co.com.sofka.domain.round.values.DiceId;
 
 public class Dice extends Entity<DiceId> {
+    private Face face;
     public Dice(DiceId entityId) {
         super(entityId);
     }
